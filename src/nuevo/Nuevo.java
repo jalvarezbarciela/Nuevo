a5e8b7b4eac2ecdb819f6b9e3a4c008f94d84a8e
jalvarezbarciela <jalvarezbarciela@nautilus6.danielcastelao.org>
2019-10-16 14:30:41 +0200

    Segundo

--- src/nuevo/Nuevo.java
+++ src/nuevo/Nuevo.java
@@ -15,7 +15,7 @@ public class Nuevo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //comentario sin sentido
     }
     
 }
